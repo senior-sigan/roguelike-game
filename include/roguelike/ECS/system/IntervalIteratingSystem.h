4ae7cc688315d3d95fb8cb6ee4ffdc1b229d9872
Ilya Siganov <ilya.blan4@gmail.com>
2018-06-05 23:16:31 +0600

    add interval systems

--- include/roguelike/ECS/system/IntervalIteratingSystem.h
+++ include/roguelike/ECS/system/IntervalIteratingSystem.h
@@ -5,4 +5,93 @@
 #ifndef ROGUELIKE_INTERVALITERATINGSYSTEM_H
 #define ROGUELIKE_INTERVALITERATINGSYSTEM_H
 
+#include <ECS/EntityManager.h>
+#include "ECS/FamilyTypeID.h"
+#include "ISystem.h"
+namespace ECS {
+
+/**
+ * It's a combination of interval and iterating systems.
+ *
+ * @tparam T
+ */
+template<class T>
+class IntervalIteratingSystem : public ISystem {
+  friend class SystemManager;
+  static const SystemTypeID STATIC_TYPE_ID;
+  const double interval{};
+  double currentTime{};
+
+  template<class T>
+  void PreUpdate(double dt) {
+      currentTime += dt; // Every PRE update increment timer
+      if (currentTime >= interval) {
+          PreUpdateInterval(currentTime);
+      }
+  }
+  template<class T>
+  void Update(double dt) {
+      if (currentTime >= interval) {
+          UpdateInterval(currentTime);
+      }
+  }
+  template<class T>
+  void PostUpdate(double dt) {
+      if (currentTime >= interval) {
+          PostUpdateInterval(currentTime);
+          currentTime -= interval;  // Every POST update decrement timer
+      }
+  }
+
+  void PreUpdateInterval(double dt) {
+      for (auto entity : GetEntityManager()->container) {
+          if (FamilyFilter(entity.second)) {
+              PreProcessEntity(entity.second, dt);
+          }
+      }
+  }
+  void UpdateInterval(double dt) {
+      for (auto entity : GetEntityManager()->container) {
+          if (FamilyFilter(entity.second)) {
+              ProcessEntity(entity.second, dt);
+          }
+      }
+  }
+  void PostUpdateInterval(double dt) {
+      for (auto entity : GetEntityManager()->container) {
+          if (FamilyFilter(entity.second)) {
+              PostProcessEntity(entity.second, dt);
+          }
+      }
+  }
+
+ public:
+  template<class T>
+  explicit IntervalIteratingSystem(const double interval):interval(interval) {
+      currentTime = 0;
+  }
+
+  const SystemTypeID GetTypeID() const override {
+      return STATIC_TYPE_ID;
+  }
+
+  virtual void PreProcessEntity(IEntity *entity, double dt) {};
+  virtual void ProcessEntity(IEntity *entity, double dt) {};
+  virtual void PostProcessEntity(IEntity *entity, double dt) {};
+
+  /**
+   * Filter and pass only IEntities that satisfy the predicate.
+   * @return true if satisfy and false otherwise.
+   */
+  virtual const bool FamilyFilter(IEntity *entity) const {
+      return false;
+  };
+};
+
+template<class T>
+const ECS::SystemTypeID
+    ECS::IntervalIteratingSystem<T>::STATIC_TYPE_ID = ECS::Internal::FamilyTypeID<ECS::ISystem>::Get<T>();
+
+}
+
 #endif //ROGUELIKE_INTERVALITERATINGSYSTEM_H
--- include/roguelike/ECS/system/IntervalSystem.h
+++ include/roguelike/ECS/system/IntervalSystem.h
@@ -5,4 +5,62 @@
 #ifndef ROGUELIKE_INTERVALSYSTEM_H
 #define ROGUELIKE_INTERVALSYSTEM_H
 
+#include "ECS/FamilyTypeID.h"
+#include "ISystem.h"
+namespace ECS {
+
+/**
+ * Update commands will be called on every world update, but after given interval.
+ * If you need iterates over Entities, use IteratingSystem.
+ *
+ * @tparam T
+ */
+template<class T>
+class IntervalSystem : public ISystem {
+  friend class SystemManager;
+  static const SystemTypeID STATIC_TYPE_ID;
+  const double interval{};
+  double currentTime{};
+
+  template<class T>
+  void PreUpdate(double dt) {
+      currentTime += dt; // Every PRE update increment timer
+      if (currentTime >= interval) {
+          PreUpdateInterval(currentTime);
+      }
+  }
+  template<class T>
+  void Update(double dt) {
+      if (currentTime >= interval) {
+          UpdateInterval(currentTime);
+      }
+  }
+  template<class T>
+  void PostUpdate(double dt) {
+      if (currentTime >= interval) {
+          PostUpdateInterval(currentTime);
+          currentTime -= interval;  // Every POST update decrement timer
+      }
+  }
+
+ public:
+  template<class T>
+  explicit IntervalSystem(const double interval):interval(interval) {
+      currentTime = 0;
+  }
+
+  const SystemTypeID GetTypeID() const override {
+      return STATIC_TYPE_ID;
+  }
+
+  virtual void PreUpdateInterval(double dt) {};
+  virtual void UpdateInterval(double dt) {};
+  virtual void PostUpdateInterval(double dt) {};
+};
+
+template<class T>
+const ECS::SystemTypeID ECS::IntervalSystem<T>::STATIC_TYPE_ID = ECS::Internal::FamilyTypeID<ECS::ISystem>::Get<T>();
+
+}
+
 #endif //ROGUELIKE_INTERVALSYSTEM_H
