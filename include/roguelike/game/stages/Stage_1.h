35de388063f31b9f6c272248c40448318d6bef74
Ilya Siganov <ilya.blan4@gmail.com>
2018-07-03 01:15:56 +0600

    add lvl

--- include/roguelike/game/stages/Stage_1.h
+++ include/roguelike/game/stages/Stage_1.h
@@ -16,7 +16,9 @@
 #include <game/systems/ItemGatheringSystem.h>
 #include <game/entities/CoinEntity.h>
 #include "IStage.h"
+#include "lvl1.h"
 class Stage_1 : public IStage {
+  LOG_INIT("Stage_1");
  public:
   ECS::Engine *const load() override {
       // TODO: By the way, we can load them from config files!
@@ -30,18 +32,29 @@ class Stage_1 : public IStage {
       engine->GetSystemManager()->CreateAndGet<ItemGatheringSystem>();
 
       engine->GetEntityManager()->CreateAndGet<PlayerEntity>(Core::Vector2(1, 1));
-      engine->GetEntityManager()->CreateAndGet<WallEntity>(Core::Vector2(5, 2));
-      engine->GetEntityManager()->CreateAndGet<WallEntity>(Core::Vector2(5, 3));
-      engine->GetEntityManager()->CreateAndGet<WallEntity>(Core::Vector2(5, 4));
-      engine->GetEntityManager()->CreateAndGet<WallEntity>(Core::Vector2(5, 6));
-      engine->GetEntityManager()->CreateAndGet<WallEntity>(Core::Vector2(5, 7));
 
-      engine->GetEntityManager()->CreateAndGet<CoinEntity>(Core::Vector2(3,3), 1);
-      engine->GetEntityManager()->CreateAndGet<CoinEntity>(Core::Vector2(6,3), 1);
-      engine->GetEntityManager()->CreateAndGet<CoinEntity>(Core::Vector2(1,8), 1);
-      engine->GetEntityManager()->CreateAndGet<CoinEntity>(Core::Vector2(4,1), 1);
+      for (int x = 0; x < lvl::width; x++) {
+          for (int y = 0; y < lvl::height; y++) {
+              createEntity(engine, x, y, lvl::data[y*lvl::width + x]);
+          }
+      }
 
       return engine;
+  }
+
+  void createEntity(ECS::Engine *const engine, int x, int y, char tile) {
+      // TODO: create a configuration file that map tiles to entities and remove switch
+      switch (tile) {
+          case 0: {
+              engine->GetEntityManager()->CreateAndGet<WallEntity>(Core::Vector2(x, y));
+              break;
+          }
+          case 1: {
+              engine->GetEntityManager()->CreateAndGet<CoinEntity>(Core::Vector2(x, y), 1);
+              break;
+          }
+          default:break;
+      }
   }
 };
 
